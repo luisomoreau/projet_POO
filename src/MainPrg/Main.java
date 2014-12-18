653f2b08f3fe3b40391cf9dc4ee31f11f7abeea3
Louis <louis.moreau@hei.fr>
2014-12-18 19:28:06 +0100

    feat - built skeleton view
    View.App :
    -> initialized the main window
    -> set the main JPanel with header
    
    Issues : menu bar does not appear in window neither the main panel
    
    Todo : fix the last issues to show menu bar + main panel

--- src/MainPrg/Main.java
+++ src/MainPrg/Main.java
@@ -1,7 +1,13 @@
 package MainPrg;
 
+import View.App;
+
 /**
  * Created by Louis on 18/12/2014.
  */
 public class Main {
+    public static void main(String[] args) {
+        App app=new App();
+
+    }
 }
--- src/View/App.java
+++ src/View/App.java
@@ -1,7 +1,65 @@
 package View;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Louis on 18/12/2014.
  */
-public class App {
+
+public class App extends JFrame{
+
+    private JMenuBar barreMenu;
+    private JMenu fichier;
+    private JMenu panier;
+    private JMenuItem voirPanier;
+    private JMenuItem quitter;
+    private JPanel mainPan;
+    private JPanel panelHeader;
+    private JButton rechercher;
+    private JTextField rechercheTextField;
+
+    public App(){
+        this.setTitle("Convertisseur de monnaie");
+        this.setSize(800, 500);
+
+        // On appelle la méthode qui retournera toute notre vue.
+        this.setContentPane(initialise() );
+
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JOptionPane.showMessageDialog(this, "Commencer mes achats");
+
+        //On ajoute un menu à la fenêtre principale
+        //Ici est initialisée la barre de menu
+        barreMenu=new JMenuBar();
+
+        //On ajoute les deux onglets "fichier" et "panier"
+        fichier=new JMenu("Fichier");
+        panier=new JMenu("Panier");
+        barreMenu.add(fichier);
+        barreMenu.add(panier);
+
+        //On ajoute les champs "Quitter" à l'onglet "Fichier" et "Voir le panier" à "Panier"
+        quitter=new JMenuItem("Quitter");
+        voirPanier= new JMenuItem("Voir panier");
+        fichier.add(quitter);
+        panier.add(voirPanier);
+
+        this.setJMenuBar(barreMenu);
+    }
+    private JPanel initialise(){
+        //On initialise le panel principal
+        mainPan = new JPanel();
+        //On initialise le premier JPanel qui contiendra le header, à savoir le bouton "rechercher" et le champs de recherche
+        panelHeader = new JPanel();
+        rechercher= new JButton("Rechercher");
+        rechercheTextField= new JTextField();
+        mainPan.add(panelHeader, BorderLayout.NORTH);
+
+        return mainPan;
+
+    }
 }
