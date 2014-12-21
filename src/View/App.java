f8698fbf887e51be1eff598d84ec66fadf13bb8c
Louis <louis.moreau@hei.fr>
2014-12-22 00:41:29 +0100

    fix - rebuilt view
    View.App :
    -> add all elements on view
    
    Issues : need to resize the page for the picture to appear...
    
    Todo : fix the last issues

--- src/View/App.java
+++ src/View/App.java
@@ -11,36 +11,34 @@ import java.awt.*;
 public class App extends JFrame{
 
     //Creation des panels
-    private JPanel mainPan = new JPanel();
-    private JPanel panelHeader = new JPanel();
-    private JPanel tablePanel = new JPanel();
-    private JPanel leftTablePanel = new JPanel();
-    private JPanel rightTablePanel = new JPanel();
-    private JPanel addToCartPanel = new JPanel();
+    JPanel mainPan = new JPanel();
+    JPanel panelHeader = new JPanel();
+    JPanel tablePanel = new JPanel();
+    JPanel leftTablePanel = new JPanel();
+    JPanel rightTablePanel = new JPanel();
+    JPanel addToCartPanel = new JPanel();
 
     //Creation de la scrollbar
-    private JScrollPane scrollPanel= new JScrollPane();
+    JScrollPane scrollPanel= new JScrollPane(mainPan);
 
     //Création du menu
-    private JMenuBar barreMenu=new JMenuBar();
-    private JMenu fichier=new JMenu("Fichier");
-    private JMenu panier=new JMenu("Panier");
-    private JMenuItem voirPanier=new JMenuItem("Quitter");
-    private JMenuItem quitter= new JMenuItem("Voir panier");
+    JMenuBar barreMenu=new JMenuBar();
+    JMenu fichier=new JMenu("Fichier");
+    JMenu panier=new JMenu("Panier");
+    JMenuItem voirPanier=new JMenuItem("Quitter");
+    JMenuItem quitter= new JMenuItem("Voir panier");
 
     //Creation des champs de recherche
-    private JButton rechercher= new JButton("Rechercher");
-    private JTextField rechercheTextField= new JTextField("Tapez le nom du produit recherché",20);
+    JButton rechercher= new JButton("Rechercher");
+    JTextField rechercheTextField= new JTextField("Tapez le nom du produit recherché",20);
 
     //Creation des champs du tableau de gauche
-    private JLabel phoneName=new JLabel("Nom :");
-    private JLabel phonePrice= new JLabel("Prix :");
+    JLabel phoneName=new JLabel("Nom :");
+    JLabel phonePrice= new JLabel("Prix :");
 
     //Creation des champs du tableau de droite
-    private JLabel phonePictureLabel = new JLabel();
-    private ImageIcon phonePicture= new ImageIcon(new ImageIcon("img/iphone6.jpg").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT));
-    private JCheckBox addToCartCheckBox = new JCheckBox("Ajouter au panier");
-    private JButton showMoreDetails = new JButton("Détails");
+    JCheckBox addToCartCheckBox = new JCheckBox("Ajouter au panier");
+    JButton showMoreDetails = new JButton("Détails");
 
 
     public App(){
@@ -76,7 +74,7 @@ public class App extends JFrame{
         panier.add(voirPanier);
 
         // On appelle la méthode qui retournera toute notre vue.
-        this.setContentPane(mainPan);
+        this.add(mainPan);
 
         //On ajoute le panel recherche au panel principal
         mainPan.add(panelHeader, BorderLayout.NORTH);
@@ -90,10 +88,13 @@ public class App extends JFrame{
 
         //On crée un panel qui contiendra les produits
         tablePanel.setBorder(border);
-        tablePanel.setLayout(new GridLayout(0,2,10,10));
+        tablePanel.setLayout(new GridLayout(0,2));
+
+        //On ajoute le panel des produits au panel principal
+        mainPan.add(tablePanel, BorderLayout.CENTER);
 
         //On ajoute le panel de gauche avec le nom et le prix de l'article en définissant sa taille et sa position
-        leftTablePanel.setPreferredSize(new Dimension(300, 300 ));
+        leftTablePanel.setPreferredSize(new Dimension(360, 300 ));
         leftTablePanel.setLayout(new BoxLayout(leftTablePanel, BoxLayout.PAGE_AXIS));
         leftTablePanel.setBorder(border);
         tablePanel.add(leftTablePanel);
@@ -103,12 +104,14 @@ public class App extends JFrame{
         leftTablePanel.add(phonePrice);
 
         //On ajoute le panel de droite avec la photo, l'ajout au panier et les détails
-        rightTablePanel.setPreferredSize(new Dimension(300, 300));
+        rightTablePanel.setPreferredSize(new Dimension(360, 300));
         rightTablePanel.setLayout(new BoxLayout(rightTablePanel, BoxLayout.PAGE_AXIS));
         rightTablePanel.setBorder(border);
         tablePanel.add(rightTablePanel);
 
         //On ajoute le panel de la photo et celui de l'ajout au panier dans le panel de droite
+        ImageIcon phonePicture= new ImageIcon(new ImageIcon("img/iphone6.jpg").getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT));
+        JLabel phonePictureLabel = new JLabel(phonePicture);
         rightTablePanel.add(phonePictureLabel);
         rightTablePanel.add(addToCartPanel);
 
@@ -117,49 +120,4 @@ public class App extends JFrame{
         addToCartPanel.add(showMoreDetails);
 
     }
-    /*
-    private JPanel initialise(){
-
-        //On initialise le tableau qui contiendra le nom de l'article et le prix ainsi que la photo du téléphone, l'ajout au panier + le bouton de détail
-        tablePanel = new JPanel();
-        tablePanel.setPreferredSize( new Dimension( 800, 280 ) );
-        tablePanel.setLayout(new BorderLayout());
-        //tableau de gauche
-        leftTablePanel = new JPanel();
-        phoneName = new JLabel("Nom :");
-        phonePrice = new JLabel("Prix :");
-        leftTablePanel.add(phoneName);
-        leftTablePanel.add(phonePrice);
-        tablePanel.add(leftTablePanel, BorderLayout.CENTER);
-
-        //Tableau de droite
-        rightTablePanel = new JPanel();
-        phonePicture = new ImageIcon(new ImageIcon("img/iphone6.jpg").getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT));
-        phonePictureLabel = new JLabel(phonePicture);
-        addToCartPanel = new JPanel();
-        addToCartCheckBox = new JCheckBox();
-        showMoreDetails = new JButton("Details");
-        addToCartPanel.add(addToCartCheckBox);
-        addToCartPanel.add(showMoreDetails);
-        rightTablePanel.setLayout(new BorderLayout());
-        rightTablePanel.add(addToCartPanel, BorderLayout.SOUTH);
-        rightTablePanel.add(phonePictureLabel, BorderLayout.CENTER);
-        tablePanel.add(rightTablePanel, BorderLayout.EAST);
-
-
-        //On associe les bordures aux différents panel
-        tablePanel.setBorder(border);
-        rightTablePanel.setBorder(border);
-        leftTablePanel.setBorder(border);
-
-
-        //On ajoute les panels au panel principal
-        mainPan.setLayout(new BorderLayout());
-        mainPan.add(panelHeader, BorderLayout.NORTH);
-        mainPan.add(tablePanel, BorderLayout.CENTER);
-
-        return mainPan;
-
-    }
-    */
 }
